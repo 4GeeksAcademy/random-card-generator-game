be05c3f5767b28a6bae74ad1ff8db2dbe4badf44
Luper95 <luisenriqueperez2605@hotmail.com>
2025-05-09 18:13:47 +0000

    me tuve que ir

--- src/app.js
+++ src/app.js
@@ -7,6 +7,19 @@ import "./assets/img/4geeks.ico";
 const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 const cardSuits = ['♦', '♥', '♠', '♣'];
 
+//player
+
+window.player = {
+   topIcon : document.querySelector('.card-top-icon h1'),
+   centerValue : document.querySelector('.card-number h1'),
+   bottomIcon : document.querySelector('.card-footer-icon h1'),
+};
+
+window.pc ={
+   topIcon : document.getElementById('pcCardTop'),
+   centerValue : document.getElementById('pcCardNumber'),
+   bottomIcon : document.getElementById('pcCardBot'),
+}
 const getRandomIndex = (length) => {
   return Math.floor(Math.random() * length);
 }
@@ -19,16 +32,12 @@ const generateRandomCard = () => {
     value
   }
 }
-
-window.generateCard = function () {
+//carta al jugador.
+window.generateCard = function (player) {
   const card = generateRandomCard();
-  const topIcon = document.querySelector('.card-top-icon h1');
-  const centerValue = document.querySelector('.card-number h1');
-  const bottomIcon = document.querySelector('.card-footer-icon h1');
-
-  topIcon.textContent = card.suit;
-  bottomIcon.textContent = card.suit;
-  centerValue.textContent = card.value;
+    player.topIcon.textContent = card.suit;
+  player.bottomIcon.textContent = card.suit;
+  player.centerValue.textContent = card.value;
 
   if (card.suit === '♥') {
     topIcon.classList.add('heart');
@@ -39,16 +48,18 @@ window.generateCard = function () {
   }
 }
 
-window.adjustCardSize = function () {
-  const width = document.getElementById("cardWidth").value;
-  const height = document.getElementById("cardHeight").value;
-  const cardElement = document.querySelector(".card");
 
-  if (width) cardElement.style.width = `${width}px`;
-  if (height) cardElement.style.height = `${height}px`;
-}
+// window.adjustCardSize = function () {
+//   const width = document.getElementById("cardWidth").value;
+//   const height = document.getElementById("cardHeight").value;
+//   const cardElement = document.querySelector(".card");
+
+//   if (width) cardElement.style.width = `${width}px`;
+//   if (height) cardElement.style.height = `${height}px`;
+// }
 
 window.onload = function () {
-  setInterval(generateCard, 10000);
-  generateCard();
+  setInterval(generateCard(pc), 1000);
+  console.log(generateCard(pc));
+  generateCard(pc);
 }
